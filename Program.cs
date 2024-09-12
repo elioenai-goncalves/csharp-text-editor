ec9bd9ad617c26aa370ad64605d2ba55f2c98cd6
Elioenai.Gonçalves <elioenai.andrade@hotmail.com>
2024-09-12 20:49:54 -0300

    adicionando projeto completo

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("O que você deseja fazer?");
+    Console.WriteLine("1 - Abrir o arquivo");
+    Console.WriteLine("2 - Criar novo arquivo");
+    Console.WriteLine("0 - Sair");
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0: Environment.Exit(0); break;
+        case 1: Open(); break;
+        case 2: Edit(); break;
+        default: Menu(); break;
+    }
+}
+
+static void Open()
+{
+    Console.Clear();
+    Console.WriteLine("Qual o caminho do arquivo?");
+    string path = Console.ReadLine();
+
+    using (var file = new StreamReader(path))
+    {
+        string text = file.ReadToEnd();
+        Console.WriteLine(text);
+    };
+
+    Console.WriteLine("");
+    Console.ReadLine();
+    Menu();
+}
+
+static void Edit()
+{
+    Console.Clear();
+    Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+    Console.WriteLine("-------------------------");
+    string text = "";
+
+    do
+    {
+        text += Console.ReadLine();
+        text += Environment.NewLine;
+    }
+    while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+    Salvar(text);
+}
+
+static void Salvar(string text)
+{
+    Console.Clear();
+    Console.WriteLine("Qual o caminho para salvar o aquivo?");
+    var path = Console.ReadLine();
+
+    using (var file = new StreamWriter(path))
+    {
+        file.Write(text);
+    }
+
+    Console.WriteLine($"Arquivo {path} salvo com sucesso");
+    Console.ReadLine();
+    Menu();
+}
+
+
